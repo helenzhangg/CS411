8114b74d5607f677a34522ac81e1210ead419055
Helen Zhang <hz96@bu.edu>
2017-12-02 15:22:31 -0500

    added javascrs for moving cursor

---
+++ Node/nodetest1/public/javascripts/typed.js
@@ -0,0 +1,41 @@
+
+
+// script.
+//   var typed = new Typed('.element', {strings: ["First sentence.", "Second sentence."], typeSpeed: 30});
+
+
+$(function(){
+	$(".typed").typed({
+		strings: ["Hello, I am Woku :)", "I can help you explore places in your budget", "Try me out!"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 1,
+		// show cursor
+		showCursor: true,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
