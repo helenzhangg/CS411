c75b9fc45bfaa2bf6409390949e968c01f4b50bd
Jiadong Chen <chenjd@bu.edu>
2017-12-05 00:05:52 +0000

    yelp

--- Node/nodetest1/routes/index.js
+++ Node/nodetest1/routes/index.js
@@ -95,7 +95,7 @@ router.post('/api', function (req,res) {
     // EVENTBRITE API CALL
     var url_stem = 'https://www.eventbriteapi.com/v3/events/search/?token=UR24GALENHRRPDT5BSZ3';
     var location = '&location.address=' +city + ',' + state + '.' + zip;
-
+    
     // add 'price' to api_call
     // if price = 1 or 2 use price = 'free'
     if (price==1 | price ==2){
@@ -134,7 +134,31 @@ router.post('/api', function (req,res) {
         // console.log(body.url);
         // console.log(body.explanation);
     });
+    var url_yelp = 'https://api.yelp.com/v3/businesses/search?term=' + activity + '&location='+ city +'&price=' + price + '&Authorization=Bearer2rrk6a3kWZLBoZs2d1AVDu3ui3FEVX0trMCDwSzJm7CLE1julO2TBszCxlQCL3siV34fBvPFjL2E08dwwXNCGL3QRibByfKMj3wTXBZJ2oP2uPIG8XYN_0EOzrvyWXYx&sort_by=distance';
+    console.log(url_yelp);
+    var evt_name_yelp = [];              //Event Name
+    var evt_venue_yelp = [];             //Event venue id
+    // var evt_logo = [];              //Event logo
+    var evt_catgr_yelp = [];               //Event description
+    var evt_rating_yelp = [];               //Event start time
+    var evt_url_yelp = [];               //Event url
+    request(url_yelp, {json:true}, function(error,response,body) {
+        if (!error && response.statusCode == 200){
+            for (let item of body.events) {             //for loop append each particular value into variables
+                evt_name_yelp.push(item.name.text);
+                evt_venue_yelp.push(item.location.display_address);
+                // evt_logo.push(item.logo.original);               //supernatural bug, cannot help with it
+                evt_catgr_yelp.push(item.categories.title;
+                evt_rating_yelp.push(item.rating);
+                evt_url_yelp.push(item.image_url);
 
+            }
+            // console.log(evt_logo)                //console for testing output of particular variables
+        }
+        if (error) { return console.log(error); }
+        // console.log(body.url);
+        // console.log(body.explanation);
+    });
 
 });
 
